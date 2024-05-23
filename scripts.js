38e5fa665516440ebda867fa46fbe72858cdf896
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-22 23:04:07 -0400

    (#3) Use Case: create new `operate` function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
-// initialize relevant variables to null for complete erasing of the memory
+// initialize main variables to null for complete erasing of the memory
 let firstNumber = null;
 let operator = null;
 let secondNumber = null;
 
+// basic arithmetical function declarations for clarity
 function add(a, b) {
   return a + b;
 }
@@ -17,4 +18,23 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(op, a, b) {
+  a = parseFloat(a); // process string to number before computation,
+  b = parseFloat(b); // without affecting the main calculator variables
+
+  if (op === "+") {
+    return add(a, b);
+  } else if (op === "-") {
+    return subtract(a, b);
+  } else if (op === "*") {
+    return multiply(a, b);
+  } else if (op === "/") {
+    return divide(a, b);
+  } else if (op === "**") {
+    return power(a, b);
+  } else if (op === "%") {
+    return percentage(a, b);
+  }
 }
