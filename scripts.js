222b023bd1068c056da6aebc184ca62740c22a19
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-22 22:50:43 -0400

    (#2) Use Case: create variables for display update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// initialize relevant variables to null for complete erasing of the memory
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+
 function add(a, b) {
   return a + b;
 }
