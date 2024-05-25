75b2370309c57b155fb7f725c00eb2415e8c0faf
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-25 10:00:58 -0400

    Start adding memory functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const numberButtons = document.querySelectorAll(".button.number");
 const specialButtons = document.querySelectorAll(".button.special");
 const operatorButtons = document.querySelectorAll(".button.operator");
+const memoryButtons = document.querySelectorAll(".button.memory");
 const displayText = document.getElementById("displayText");
 
 let firstNumber = null;
@@ -10,7 +11,7 @@ let lastOperator = null;
 let lastSecondNumber = null;
 let awaitingNewInput = null;
 let repeatLastOperation = false;
-let memoryNumber = 0;
+let memoryNumber = "";
 let displayValue = "";
 
 function add(a, b) {
@@ -42,22 +43,22 @@ function applyPercentage(operator, baseValue, percentageValue) {
   let percentageDecimal = percentageValue / 100;
   switch (operator) {
     case '+':
-      case '-':
-        result = baseValue * percentageDecimal;
-        return operator === '-' ? baseValue - result : baseValue + result;
-        case '*':
-          result = baseValue * percentageDecimal;
-          return result;
-          case '/':
-            result = baseValue / percentageDecimal;
-            return result;
-            default:
-              return;
-            }
-          }
-          
-          function operate(op, a, b) {
-            a = parseFloat(a);
+    case '-':
+      result = baseValue * percentageDecimal;
+      return operator === '-' ? baseValue - result : baseValue + result;
+    case '*':
+      result = baseValue * percentageDecimal;
+      return result;
+    case '/':
+      result = baseValue / percentageDecimal;
+      return result;
+    default:
+      return;
+  }
+}
+
+function operate(op, a, b) {
+  a = parseFloat(a);
   b = parseFloat(b);
 
   if (op === "+") {
@@ -79,6 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setupButtonListeners(numberButtons);
   setupButtonListeners(specialButtons);
   setupButtonListeners(operatorButtons);
+  setupButtonListeners(memoryButtons);
   document.addEventListener('keydown', handleGlobalKeyDown);
   document.addEventListener('keyup', handleGlobalKeyUp);
 });
@@ -124,6 +126,8 @@ function handleButtonClick(button) {
     handleSpecial(value);
   } else if (button.classList.contains("operator")) {
     handleOperator(value);
+  } else if (button.classList.contains("memory")) {
+    handleMemory(value);
   }
 }
 
@@ -182,6 +186,31 @@ function handleSpecial(specialValue) {
   }
 }
 
+function handleOperator(symbol) {
+  if (symbol === "sqrt") {
+    handleSqrtOperation();
+  } else if (symbol === '%' && operator !== "%" && firstNumber !== null && displayValue !== "") {
+    handlePercentageOperation();
+  } else if (symbol === null && repeatLastOperation) {
+    handleRepeatLastOperation();
+  } else {
+    handleStandardOperation(symbol);
+  }
+
+  if (symbol === null) {
+    repeatLastOperation = true;
+  }
+  awaitingNewInput = true;
+}
+
+function handleMemory(memory) {
+  if (memory === "M+") {
+    memoryNumber += displayValue;
+  } else if (memory === "M-") {
+    memoryNumber -+ displayValue;
+  }
+}
+
 function handleSqrtOperation() {
   let result = Math.sqrt(firstNumber);
   displayValue = result.toString();
@@ -226,23 +255,6 @@ function handleStandardOperation(symbol) {
     displayValue = "";
   }
   operator = symbol;
-}
-
-function handleOperator(symbol) {
-  if (symbol === "sqrt") {
-    handleSqrtOperation();
-  } else if (symbol === '%' && operator !== "%" && firstNumber !== null && displayValue !== "") {
-    handlePercentageOperation();
-  } else if (symbol === null && repeatLastOperation) {
-    handleRepeatLastOperation();
-  } else {
-    handleStandardOperation(symbol);
-  }
-
-  if (symbol === null) {
-    repeatLastOperation = true;
-  }
-  awaitingNewInput = true;
 }
 
 function formatResult(result) {
