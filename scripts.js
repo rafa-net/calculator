3d0b96633a8275a2c6290f035fc3deefce52929b
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-25 04:44:55 -0400

    Organize code and establish keys for all buttons

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ let lastOperator = null;
 let lastSecondNumber = null;
 let awaitingNewInput = null;
 let repeatLastOperation = false;
-let memory = 0;
+let memoryNumber = 0;
 let displayValue = "";
 
 function add(a, b) {
@@ -42,22 +42,22 @@ function applyPercentage(operator, baseValue, percentageValue) {
   let percentageDecimal = percentageValue / 100;
   switch (operator) {
     case '+':
-    case '-':
-      result = baseValue * percentageDecimal;
-      return operator === '-' ? baseValue - result : baseValue + result;
-    case '*':
-      result = baseValue * percentageDecimal;
-      return result;
-    case '/':
-      result = baseValue / percentageDecimal;
-      return result;
-    default:
-      return;
-  }
-}
-
-function operate(op, a, b) {
-  a = parseFloat(a);
+      case '-':
+        result = baseValue * percentageDecimal;
+        return operator === '-' ? baseValue - result : baseValue + result;
+        case '*':
+          result = baseValue * percentageDecimal;
+          return result;
+          case '/':
+            result = baseValue / percentageDecimal;
+            return result;
+            default:
+              return;
+            }
+          }
+          
+          function operate(op, a, b) {
+            a = parseFloat(a);
   b = parseFloat(b);
 
   if (op === "+") {
@@ -84,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function handleGlobalKeyDown(e) {
+  console.log("Key pressed: " + e.key);
   const buttonPressed = document.querySelector(`.button[data-key="${e.key}"]`);
   if (buttonPressed) {
     buttonPressed.click();
